fd5151d5be7f62540071a683453ad67f93f32628
Bartosz Piatek <your.email@example.com>
2025-09-29 13:44:49 +0200

    do not limit custom shortUrl to 7 characters

--- src/main/java/pl/bpiatek/linkshortenerlinkservice/api/dto/CreateLinkRequest.java
+++ src/main/java/pl/bpiatek/linkshortenerlinkservice/api/dto/CreateLinkRequest.java
@@ -1,18 +1,16 @@
 package pl.bpiatek.linkshortenerlinkservice.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import pl.bpiatek.linkshortenerlinkservice.api.ValidUrl;
 
 public record CreateLinkRequest(
         @NotBlank(message = "The destination URL cannot be blank.")
         @ValidUrl(message = "A valid URL format is required.")
         String longUrl,
-        @Size(max = 7, message = "Custom short URL cannot be longer than {max} characters.")
         String shortUrl,
         Boolean isActive) {
-        @Override
 
+        @Override
         public Boolean isActive() {
                 return this.isActive == null || isActive;
         }
