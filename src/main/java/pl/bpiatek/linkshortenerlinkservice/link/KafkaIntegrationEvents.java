da8afe8da0d3a7f96aed18b3cd0942b99e55a4a9
Bartosz Piatek <your.email@example.com>
2025-09-29 13:18:15 +0200

    use virtual threads with tomcat and kafka  producer

--- src/main/java/pl/bpiatek/linkshortenerlinkservice/link/KafkaIntegrationEvents.java
+++ src/main/java/pl/bpiatek/linkshortenerlinkservice/link/KafkaIntegrationEvents.java
@@ -1,5 +1,6 @@
 package pl.bpiatek.linkshortenerlinkservice.link;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -11,6 +12,7 @@ class KafkaIntegrationEvents {
         this.kafkaProducerService = kafkaProducerService;
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     void handleLinkCreatedEvent(LinkCreatedApplicationEvent event) {
         kafkaProducerService.sendLinkCreatedEvent(event.link());
