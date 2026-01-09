3db394b9f21148b709bbd43080fa1499654133e9
Bartosz Piatek <your.email@example.com>
2026-01-09 13:17:51 +0100

    limit short url to contain only specific set of characters

--- src/main/java/pl/bpiatek/linkshortenerlinkservice/api/dto/CreateLinkRequest.java
+++ src/main/java/pl/bpiatek/linkshortenerlinkservice/api/dto/CreateLinkRequest.java
@@ -1,6 +1,7 @@
 package pl.bpiatek.linkshortenerlinkservice.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import pl.bpiatek.linkshortenerlinkservice.api.ValidUrl;
 
@@ -9,6 +10,7 @@ public record CreateLinkRequest(
         @ValidUrl(message = "A valid URL format is required.")
         @Size(max = 1024, message = "The destination URL cannot exceed 1024 characters.")
         String longUrl,
+        @Pattern(regexp = "^[a-zA-Z0-9-_]+$", message = "Short URL can only contain alphanumeric characters, hyphens, and underscores.")
         String shortUrl,
         Boolean isActive,
         @Size(max = 255, message = "The title cannot exceed 255 characters.")
